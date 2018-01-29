e87c989bb18c0f34de37d4502891c7090739a12a
Alexander Davydov <dabbidob.ajiekc@gmail.com>
2018-01-29 17:44:00 +0700

    29.01

--- Parser/Form1.cs
+++ Parser/Form1.cs
@@ -9,6 +9,7 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using Classes;
 using System.Text.RegularExpressions;
+using System.Xml;
 
 
 
@@ -22,25 +23,32 @@ namespace Parser
             HTMLWorker wrk = new HTMLWorker();
             try
             {
-               // Uri e = new Uri(@"rt[FULLNAME]=1");//@"D:\1.txt"   @""   @"D:\chm\1234.htm" D:\Prikaz.html
+                //Uri e = new Uri(@"https://www.gismeteo.ru/weather-novosibirsk-4690/now/");//@"D:\1.txt"   @""   @"D:\chm\1234.htm" D:\Prikaz.html  @"http:\\www.google.com"
 
                 //string str = HTMLWorker.GetHtml(e, Encoding.UTF8);
-                //HTMLWorker.SaveFile(@"D:\1.html", str);
-                string str = HTMLWorker.GetHtml(@"D:\1.html", Encoding.UTF8);
-                wrk.RemoveBlock(ref str, HTMLWorker.P_SCRIPT);
-                wrk.RemoveBlock(ref str, HTMLWorker.GetPattern_PAIRED_TAG("style"));
+                //HTMLWorker.SaveFile(@"D:\4.html", str);
+                string str = HTMLWorker.GetHtml(@"D:\4.html", Encoding.UTF8);
                 wrk.RemoveBlock(ref str, HTMLWorker.GetPattern_PAIRED_TAG("head"));
+                MatchCollection aaa=wrk.RemoveBlock(ref str, HTMLWorker.P_SCRIPT);
+                wrk.RemoveBlock(ref str, HTMLWorker.GetPattern_PAIRED_TAG("style"));            
                 wrk.RemoveBlock(ref str, HTMLWorker.P_COMMENT);
 
-                
+                wrk.SetResolvedAttributes(new List<string>() { "class", "id","href","src" });
                 List<Tag> tags = wrk.Split(str);
+                List<Dictionary<string, string>> sss = new List<Dictionary<string, string>>();
                 foreach (Tag row in tags)
                 {
                     string tagToTag = row.Value;
                     
-                    Dictionary<string, string> ssss = wrk.GetAttributes(tagToTag);
+                    sss.Add( wrk.GetAttributes(tagToTag));
 
                 }
+                List<Tag> correctedtags = wrk.FindSingleTags(str);
+                string newHtml = wrk.CompileHtml(correctedtags);
+
+                XmlDocument xm = new XmlDocument();
+                xm.LoadXml(newHtml);
+
 
                 //string table = wrk.GetMatches(str, HTMLWorker.GetPattern_PAIRED_TAG("table"))[0].Value;
                 //List<string> dev = wrk.Split(str);
